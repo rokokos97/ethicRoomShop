7928fff135fab80aeb108e67406abf90da590722
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-03-16 16:20:06 +0100

    itemsListPage.jsx => add block with spinner (11-15)

--- src/components/itemsListPage.jsx
+++ src/components/itemsListPage.jsx
@@ -7,30 +7,34 @@ const ItemsListPage = () => {
     api.items.fetchAll().then((data) => setItems(data));
   }, []);
   console.log(items);
-  return ( items &&
-    <div>
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">name</th>
-            <th scope="col">price</th>
-            <th scope="col">description</th>
-            <th scope="col">composition</th>
-          </tr>
-        </thead>
-        <tbody>
-          { items.map((item) =>
-            <tr key={item.id}>
-              <td>{item.name}</td>
-              <td>{item.price}</td>
-              <td>{item.description}</td>
-              <td>composititon arr</td>
-            </tr>,
-          )}
+  return ( items.length === 0 ?
+      <div
+        className="spinner-grow text-dark d-flex m-5 mx-auto"
+        role="status"
+      /> :
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">name</th>
+              <th scope="col">price</th>
+              <th scope="col">description</th>
+              <th scope="col">composition</th>
+            </tr>
+          </thead>
+          <tbody>
+            { items.map((item) =>
+              <tr key={item.id}>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.description}</td>
+                <td>composititon arr</td>
+              </tr>,
+            )}
 
-        </tbody>
-      </table>
-    </div>
+          </tbody>
+        </table>
+      </div>
   );
 };
 
