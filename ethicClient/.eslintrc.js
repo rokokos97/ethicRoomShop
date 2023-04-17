dac60280a68ad255ff114d0052874bbac73f8c12
Rostyslav Lisovyi <rokokos97@gmail.com>
2023-04-17 19:56:04 +0200

    merge all files to master

--- ethicClient/.eslintrc.js
+++ ethicClient/.eslintrc.js
@@ -17,14 +17,17 @@ module.exports = {
     'react',
   ],
   'rules': {
-    'require-jsdoc': ['error', {
-      'require': {
-        'FunctionDeclaration': false,
-        'MethodDefinition': false,
-        'ClassDeclaration': false,
-        'ArrowFunctionExpression': false,
-        'FunctionExpression': false,
+    'require-jsdoc': [
+      'error',
+      {
+        require: {
+          FunctionDeclaration: false,
+          MethodDefinition: false,
+          ClassDeclaration: false,
+          ArrowFunctionExpression: false,
+          FunctionExpression: false,
+        },
       },
-    }],
+    ],
   },
 };
